96978af0a4f462c23b0206feccf2dd59a6e6ca10
Ashwani Sharma <i14a23h19a@gmail.com>
2021-06-17 20:35:59 +0530

    chore: Renamed queen main class

--- queen/src/main/java/com/seacreeper/queen/QueenApplication.java
+++ queen/src/main/java/com/seacreeper/queen/QueenApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class QueenApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(QueenApplication.class, args);
   }
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(Application.class);
+    return application.sources(QueenApplication.class);
   }
 }
